d3f4de05b97aebdadd6868388da186490ccae59b
AmineBNLK <amine.bnlk@gmail.com>
2023-12-14 14:52:59 +0100

    Inital Express setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Body parser
+//app.use(express.json());
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
